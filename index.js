542bfa47f5b170c2027c93255a8cfd2a6caed8ed
Damian Framke <damian.framke@neuland-bfi.de>
2018-09-13 22:04:23 +0200

    0.1.0

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
+/* eslint-disable no-unused-vars */
 'use strict';
-const os       = require('os');
-const fs       = require('fs');
-const del      = require('del');
-const alfy     = require('alfy');
-const tempDir  = require('temp-dir');
+const os = require('os');
+const fs = require('fs');
+const del = require('del');
+const alfy = require('alfy');
+const tempDir = require('temp-dir');
 const download = require('download');
 
-const q            = `q=${alfy.input}`;
+const q = `q=${alfy.input}`;
 const tempImageDir = `${os.tmpdir()}/alfred-google-books/`;
 
 alfy.fetch('https://www.googleapis.com/books/v1/volumes', {
-		method: 'GET',
-		query : {
-				q
-		}
+	method: 'GET',
+	query: {
+		q
+	}
 })
 .then(data => {
-		if (!fs.existsSync(tempImageDir)){
-				fs.mkdirSync(tempImageDir);
-		}
+	if (!fs.existsSync(tempImageDir)) {
+		fs.mkdirSync(tempImageDir);
+	}
 
-		const items = data.items
-		.map(item => {
-				return {
-						arg          : item.volumeInfo.previewLink,
-						title        : item.volumeInfo.title,
-						subtitle     : item.volumeInfo.authors + ' | ' + item.volumeInfo.publishedDate,
-						quicklookurl : item.volumeInfo.imageLinks.thumbnail,
-						icon : {
-								path : `${tempImageDir}${item.id}.jpg`
-						}
-				};
-		})
+	const items = data.items
+	.map(item => {
+		return {
+			arg: item.volumeInfo.previewLink,
+			title: item.volumeInfo.title,
+			subtitle: item.volumeInfo.authors + ' | ' + item.volumeInfo.publishedDate,
+			quicklookurl: item.volumeInfo.imageLinks.thumbnail,
+			icon: {
+				path: `${tempImageDir}${item.id}.jpg`
+			}
+		};
+	});
 
-		data.items.map(item => {
-				download(item.volumeInfo.imageLinks.thumbnail).then(data => {
-						fs.writeFileSync(`${tempImageDir}${item.id}.jpg`, data);
-				});
+	data.items.map(item => {
+		return download(item.volumeInfo.imageLinks.thumbnail).then(data => {
+			fs.writeFileSync(`${tempImageDir}${item.id}.jpg`, data);
 		});
+	});
 
-		alfy.output(items);
-		del([`${tempImageDir}*.jpg`], { force : true });
+	alfy.output(items);
+	del([`${tempImageDir}*.jpg`], {force: true});
 });
--- test.js
+++ test.js
@@ -1,14 +1,18 @@
+/* eslint-disable no-script-url */
 import test from 'ava';
 import alfyTest from 'alfy-test';
 
 test(async t => {
 	const alfy = alfyTest();
-	const result = await alfy('Rainbow');
+	const result = await alfy('JavaScript The Good Parts');
 
-	t.deepEqual(result, [
-		{
-			title: 'Unicorn',
-			subtitle: 'Rainbow'
+	t.deepEqual(result[0], {
+		arg: 'http://books.google.de/books?id=PXa2bby0oQ0C&pg=PA66&dq=q%3DJavaScript+The+Good+Parts&hl=&cd=1&source=gbs_api',
+		title: 'JavaScript: The Good Parts',
+		subtitle: 'Douglas Crockford | 2008-05-08',
+		quicklookurl: 'http://books.google.com/books/content?id=PXa2bby0oQ0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+		icon: {
+			path: '/tmp/alfred-google-books/PXa2bby0oQ0C.jpg'
 		}
-	]);
+	});
 });
