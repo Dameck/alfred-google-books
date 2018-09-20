cd41e297cd6cf11b3e8b3ff947a0523300bc6c11
Damian Framke <damian.framke@neuland-bfi.de>
2018-09-20 18:40:31 +0200

    Handling no results + minor tweaks

--- index.js
+++ index.js
@@ -3,50 +3,63 @@
 const os = require('os');
 const fs = require('fs');
 const del = require('del');
-const alfy = require('alfy');
 const join = require('join-array');
 const tempDir = require('temp-dir');
 const download = require('download');
+const {fetch, input, output} = require('alfy');
 
-const q = `q=${alfy.input}`;
-const tempImageDir = `${os.tmpdir()}/alfred-google-books/`;
-
-function buildSubtitle(info) {
-	const list = [info.authors, info.publisher, info.publishedDate];
-	const subtitle = list.filter(Boolean);
-	return join(subtitle, ' | ', ' | ');
-}
-
-alfy.fetch('https://www.googleapis.com/books/v1/volumes', {
+const q = `q=${input}`;
+const url = 'https://www.googleapis.com/books/v1/volumes';
+const tmp = `${os.tmpdir()}/alfred-google-books/`;
+const options = {
 	method: 'GET',
 	query: {
 		q
 	}
-})
-.then(data => {
-	if (!fs.existsSync(tempImageDir)) {
-		fs.mkdirSync(tempImageDir);
-	}
+};
 
-	const items = data.items
-		.map(item => {
+const buildSubtitle = item => {
+	const list = [item.volumeInfo.authors, item.volumeInfo.publisher, item.volumeInfo.publishedDate];
+	const subtitle = list.filter(Boolean);
+	return join(subtitle, ' | ', ' | ');
+};
+
+if (!fs.existsSync(tmp)) {
+	fs.mkdirSync(tmp);
+}
+
+fetch(url, options)
+	.then(data => {
+		if (data.totalItems === 0) {
+			return [{
+				title: `No Google Books were found that mached "${input}"`,
+				subtitle: 'Click to see the results for yourself',
+				arg: url + '?' + q
+			},
+			{
+				title: 'Go to website',
+				subtitle: 'https://books.google.com/',
+				arg: 'https://books.google.com/'
+			}];
+		}
+
+		data.items.map(item => {
+			return download(item.volumeInfo.imageLinks.thumbnail).then(data => {
+				fs.writeFileSync(`${tmp}${item.id}.jpg`, data);
+			});
+		});
+
+		return data.items.map(item => {
 			return {
 				arg: item.volumeInfo.previewLink,
 				title: item.volumeInfo.title,
-				subtitle: buildSubtitle(item.volumeInfo),
+				subtitle: buildSubtitle(item),
 				quicklookurl: item.volumeInfo.imageLinks.thumbnail,
 				icon: {
-					path: `${tempImageDir}${item.id}.jpg`
+					path: `${tmp}${item.id}.jpg`
 				}
 			};
 		});
-
-	data.items.map(item => {
-		return download(item.volumeInfo.imageLinks.thumbnail).then(data => {
-			fs.writeFileSync(`${tempImageDir}${item.id}.jpg`, data);
-		});
-	});
-
-	alfy.output(items);
-	del([`${tempImageDir}*.jpg`], {force: true});
-});
+	})
+	.then(output)
+	.then(() => del([`${tmp}*.jpg`], {force: true}));
